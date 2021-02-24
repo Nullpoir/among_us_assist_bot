bb167832a9fde4baeee5bcb7a2a5714fa8cb8631
nullpoir <nullpoir0805@gmail.com>
2021-02-24 18:18:19 +0900

    fix bugs

--- app/helpers/switcher/update_state.py
+++ app/helpers/switcher/update_state.py
@@ -1,8 +1,8 @@
 import redis
 
-def update_state(rc, channel_id):
+def update_state(category_id):
   rc = redis.Redis(host='localhost', port=6379)
-  key = key = f'mHiyori:gaming state { channel_id }'
+  key = key = f'mHiyori:gaming state { category_id }'
 
   if rc.exists(key):
     state = rc.get(key)
