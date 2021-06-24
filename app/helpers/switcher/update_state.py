5df66646b0504586235322d18b55dcb3fc257d82
nullpoir <nullpoir0805@gmail.com>
2021-06-24 20:34:17 +0900

    謎コード修正

--- app/helpers/switcher/update_state.py
+++ app/helpers/switcher/update_state.py
@@ -3,7 +3,7 @@ from settings.settings import *
 
 def update_state(category_id):
   rc = redis.Redis(host=REDIS_HOST, port=REDIS_PORT)
-  key = key = f'mHiyori:gaming state { category_id }'
+  key = f'mHiyori:gaming state { category_id }'
 
   if rc.exists(key):
     state = rc.get(key)
