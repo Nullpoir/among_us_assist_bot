09aa0d53e823ee764c0375ad155e7db8602c0dc6
nullpoir <nullpoir0805@gmail.com>
2024-05-26 23:18:15 +0900

    パフォーマンスチューニング

--- terraform/ecs_task_definition.tf
+++ terraform/ecs_task_definition.tf
@@ -1,7 +1,7 @@
 resource "aws_ecs_task_definition" "bot" {
   family                   = "${local.name_base}-bot"
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "512"
+  memory                   = "1024"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
